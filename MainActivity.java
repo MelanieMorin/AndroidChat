9063db4065b4792ec6e0b91f356c80974eb1fbba
melanie <mel.ep.morin@gmail.com>
2018-12-20 12:01:27 +0100

    messages

--- MainActivity.java
+++ MainActivity.java
@@ -2,56 +2,86 @@ package com.melmo.androidchat;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
+
+import com.google.gson.Gson;
+import com.melmo.androidchat.adapter.MessageAdapter;
+import com.melmo.androidchat.model.Message;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
-    OkHttpClient client;
 
-    @Override
+    OkHttpClient client;    @Override
+
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //get URL result
-        client = new OkHttpClient();
+        final EditText editTextMessageFilled = findViewById(R.id.editText);
+        ImageButton buttonSend = findViewById(R.id.imageButton);
 
-        final Request request = new Request.Builder()
-                .url("https://baconipsum.com/api/?type=meat-and-filler")
-                .build();
+        //Affichage de notre liste
+        RecyclerView recyclerViewMessage = findViewById(R.id.recyclerView);
+        recyclerViewMessage.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
+        recyclerViewMessage.setAdapter(new MessageAdapter(this));
 
-        client.newCall(request).enqueue(new Callback() {
+        buttonSend.setOnClickListener(new View.OnClickListener(){
+
             @Override
-            public void onFailure(Call call, IOException e) {}
+            public void onClick(View view) {
+                //TODO GET MESSAGE FILLED
+                String messageFilled = editTextMessageFilled.getText().toString();
+                String idUser = getSharedPreferences("user",MODE_PRIVATE).getString("id","");
+                SimpleDateFormat parser= new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+                String creationDate="Date inconnue";
+                try {
+                    creationDate = parser.parse(new Date().toString()).toString();
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
 
-            @Override
-            public void onResponse(Call call, final Response response) throws IOException {
-                Log.d("response", "onResponse:"+response.body().string());
-                runOnUiThread(new Runnable() {
+                //TODO SEND TO API
+                Message messageToSend = new Message(messageFilled, idUser, creationDate);
+                String jsonMessage = new Gson().toJson(messageToSend);
+                final Request request = new Request.Builder()
+                        .url("http://51.15.207.57/messages")
+                        .post(RequestBody.create(UserActivity.JSON, jsonMessage))
+                        .build();
+
+                client = new OkHttpClient();
+                client.newCall(request).enqueue(new Callback() {
                     @Override
-                    public void run() {
-                        try {
-                            setTextEditText(response.body().string());
-                        }
-                        catch(IOException e) {
-                            e.printStackTrace();
-                        }
+                    public void onFailure(Call call, IOException e) {
+                        Log.d("MainActivity", "onResponse: "+e.toString());
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+                        Log.d("MainActivity", "onResponse: "+response.body().toString());
                     }
                 });
+
+                //TODO ADD MESSAGE TO ADAPTER
+
             }
         });
+    }
 
-    }
-    private void setTextEditText(String texte){
-        EditText editTextSaisie = findViewById(R.id.editText);
-        editTextSaisie.setText(texte);
-    }
 }
